186b64efb8bf2967a6912a9890d38ef9bf6a494b
afcafcafc <75744716+afcafcafc@users.noreply.github.com>
2025-04-17 18:58:59 +0800

    Update build.sh

--- raspberrypi/24.10/build.sh
+++ raspberrypi/24.10/build.sh
@@ -12,7 +12,7 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - Starting build process..."
 # 定义所需安装的包列表
 PACKAGES=""
 PACKAGES="$PACKAGES curl"
-#PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
 # 服务——FileBrowser 用户名admin 密码admin
 PACKAGES="$PACKAGES luci-i18n-filebrowser-go-zh-cn"
 PACKAGES="$PACKAGES luci-app-argon-config"
