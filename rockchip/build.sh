08ffe1eaaa1fb16299e024bb82fbd435e1c1e012
afcafcafc <75744716+afcafcafc@users.noreply.github.com>
2025-05-04 14:41:39 +0800

    Update build.sh

--- rockchip/build.sh
+++ rockchip/build.sh
@@ -44,6 +44,91 @@ PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
 PACKAGES="$PACKAGES fdisk"
 PACKAGES="$PACKAGES script-utils"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
+# 定义所需安装的包列表
+PACKAGES=""
+
+
+packages=(
+  luci-i18n-airplay2-zh-cn
+  luci-i18n-alist-zh-cn
+  luci-app-argon-config
+  luci-i18n-ddns-go-zh-cn
+  luci-i18n-frpc-zh-cn
+  luci-i18n-ttyd-zh-cn
+  luci-i18n-vsftpd-zh-cn
+  luci-i18n-wol-zh-cn
+  luci-i18n-zerotier-zh-cn
+  #luci-app-autoreboot
+  luci-i18n-autoreboot-zh-cn
+  luci-i18n-firewall-zh-cn
+  luci-app-argon-config
+  luci-i18n-argon-config-zh-cn
+  luci-i18n-diskman-zh-cn
+  luci-i18n-package-manager-zh-cn
+  luci-i18n-ttyd-zh-cn
+  luci-app-openclash
+  luci-i18n-samba4-zh-cn
+  openssh-sftp-server
+  luci-i18n-dockerman-zh-cn
+  #luci-app-autoreboot-zh-cn
+  #luci-app-ipsec-vpnd
+  #luci-app-softethervpn
+  ppp-mod-pptp
+  script-utils
+  fdisk
+  xl2tpd
+  adb
+  curl
+  ntpdate
+  usbutils
+  python3
+  python3-pip
+  python3-requests  
+  kmod-usb-core
+  kmod-xfrm-interface
+  kmod-ipsec
+  kmod-ipsec4
+  kmod-ipsec6
+  iptables-mod-ipsec
+  iptables-nft
+  kmod-usb2
+  kmod-usb3
+  mt7601u-firmware
+  kmod-usb-net-rtl8152
+  kmod-usb-net-asix-ax88179
+  kmod-usb-net-asix
+  kmod-ath9k-htc
+  kmod-mt76
+  kmod-mt7601u
+  kmod-mt76x0u
+  kmod-mt76x2u
+  kmod-usb-ohci
+  kmod-usb-uhci
+  kmod-ata-ahci
+  kmod-ata-core
+  kmod-fs-ntfs
+  kmod-fs-ext4
+  kmod-usb-storage
+  kmod-usb-storage-extras
+  kmod-usb-serial
+  kmod-usb-serial-pl2303
+  kmod-usb-serial-ch341
+  kmod-usb-printer
+  kmod-usb-net-rndis
+  kmod-i2c-core
+  kmod-i2c-gpio
+  kmod-spi-dev
+  kmod-gpio-button-hotplug
+  kmod-leds-gpio
+  kmod-hid
+  kmod-hid-generic
+  kmod-sound-core
+  kmod-usb-audio
+  
+)
+for pkg in "${packages[@]}"; do
+  PACKAGES="$PACKAGES $pkg"
+done
 
 # 构建镜像
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
